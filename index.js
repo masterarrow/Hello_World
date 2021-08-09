5c38f19c61c2d6809982453ee06e1c5826bd5e5d
masterarrow <masterarrows@gmail.com>
2021-08-09 12:23:25 +0300

    Hello world

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = 3000;
+
+// Magic word :)
+const word = 'World';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Hello ${word}`);
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
